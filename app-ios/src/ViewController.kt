25bbe446e3ff77bea225a15619b38f0cea5b2387
Evgeniy Vitovtov <148564043+AnyGogin31@users.noreply.github.com>
2025-04-12 14:25:15 +0300

    feat: implement platform-specific entry points for Pixivv app

--- app-ios/src/ViewController.kt
+++ app-ios/src/ViewController.kt
@@ -1,1 +1,9 @@
-public fun ViewController() = Unit
+import androidx.compose.ui.window.ComposeUIViewController
+import io.anygogin31.pixivv.shared.entry.EntryView
+import platform.UIKit.UIViewController
+
+public fun ViewController(): UIViewController {
+    return ComposeUIViewController {
+        EntryView()
+    }
+}
