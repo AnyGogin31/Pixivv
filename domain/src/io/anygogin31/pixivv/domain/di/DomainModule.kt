56ed4cbceeb5b9d8c79f0a52177eb113af067677
Evgeniy Vitovtov <148564043+AnyGogin31@users.noreply.github.com>
2025-05-16 17:37:53 +0300

    feat: introduce data and domain modules with DI setup

--- domain/src/io/anygogin31/pixivv/domain/di/DomainModule.kt
+++ domain/src/io/anygogin31/pixivv/domain/di/DomainModule.kt
@@ -22,22 +22,11 @@
  * SOFTWARE.
  */
 
-package io.anygogin31.pixivv.shared.di
+package io.anygogin31.pixivv.domain.di
 
-import io.anygogin31.pixivv.core.remote.auth.di.CoreRemoteAuthModule
-import io.anygogin31.pixivv.core.storage.di.CoreStorageModule
-import io.anygogin31.pixivv.core.theme.di.CoreThemeModule
-import io.anygogin31.pixivv.data.di.DataModule
-import io.anygogin31.pixivv.data.remote.di.DataRemoteModule
-import io.anygogin31.pixivv.domain.di.DomainModule
 import org.koin.core.module.Module
+import org.koin.dsl.module
 
-public val PixivvModules: List<Module> =
-    listOf(
-        CoreRemoteAuthModule,
-        CoreStorageModule,
-        CoreThemeModule,
-        DataModule,
-        DataRemoteModule,
-        DomainModule,
-    )
+public val DomainModule: Module =
+    module {
+    }
